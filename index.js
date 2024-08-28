b8a0fefbf9c06cac9066532bad776874f1169347
MariaClaraOliveira01 <165787108+MariaClaraOliveira01@users.noreply.github.com>
2024-08-28 07:56:15 -0300

    oi

---
+++ index.js
@@ -0,0 +1,25 @@
+var nome= "Maria Clara";
+let idade= 16;
+const pi= 3.14;
+
+let a= 5;
+let b=10;
+let soma= a + b;
+
+let a=5;
+let b= 10;
+
+function multiplica(x, y)
+
+let produto= multiplica(a, b);
+
+let frutas=['Apple', 'Banana', 'Cherry];
+    console.log(frutas[1]);
+
+    let pessoa= {
+        primeiroNome: 'Maria Clara',
+        sobrenome: 'Oliveira'
+        age: 16
+    };
+
+    console.log(pessoa.primeiroNome);
